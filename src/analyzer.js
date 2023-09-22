f8eee2ab2e2a94dfdc737922e9b14fa436c5ef5d
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-09-22 13:14:46 -0500

    okey

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,14 +1,17 @@
-const analyzer = {  
+const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    return words.length;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length;
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
-  getAverageWordLength: (text) => {    
+  getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
   getNumberCount: (text) => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import analyzer from './analyzer.js';
+const boton = document.getElementById("reset-button");
+const textarea = document.querySelector('textarea[name="user-input"]');
 
+boton.addEventListener("click", function () {
+  if (textarea.value === "") {
+    alert("Debes ingresar algun texto para poder analizarlo.");
+  } else {
+    textarea.value = "";
+  }
+});
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
