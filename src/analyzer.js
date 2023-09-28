6cb7cec72911fbee213b0b44f971cbe01a7e2d0b
LuciaDuranD <oluciadurandiaz@gmail.com>
2023-09-28 10:39:56 -0500

    Listo

--- src/analyzer.js
+++ src/analyzer.js
@@ -2,7 +2,15 @@ const analyzer = {
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
     const words = text.split(" ");
-    return words.length;
+    let wordCount = 0;
+
+    for (const word of words) {
+      if (word.trim() !== "") {
+        wordCount++;
+      }
+    }
+
+    return wordCount;
   },
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
@@ -10,15 +18,50 @@ const analyzer = {
   },
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    let characterCount = 0;
+    for (const char of text) {
+      if (!/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/.test(char)) {
+        characterCount++;
+      }
+    }
+    return characterCount;
   },
   getAverageWordLength: (text) => {
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(" ");
+    let totalWordLength = 0;
+
+    for (const word of words) {
+      totalWordLength += word.length;
+    }
+
+    const averageWordLength = totalWordLength / words.length;
+    return isNaN(averageWordLength) ? 0 : averageWordLength;
   },
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    let numberCount = 0;
+
+    for (const char of text) {
+      if (/[0-9]/.test(char)) {
+        numberCount++;
+      }
+    }
+
+    return numberCount;
   },
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    let numberSum = 0;
+
+    const numbers = text.match(/\d+/g);
+    if (numbers) {
+      for (const number of numbers) {
+        numberSum += parseInt(number, 10);
+      }
+    }
+
+    return numberSum;
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,53 @@
-const boton = document.getElementById("reset-button");
-const textarea = document.querySelector('textarea[name="user-input"]');
+import analyzer from "./analyzer.js";
 
-boton.addEventListener("click", function () {
+const textarea = document.querySelector('textarea[name="user-input"]');
+const wordCountElement = document.querySelector('[data-testid="word-count"]');
+const characterCountElement = document.querySelector(
+  '[data-testid="character-count"]'
+);
+const characterNoSpacesCountElement = document.querySelector(
+  '[data-testid="character-no-spaces-count"]'
+);
+const numberCountElement = document.querySelector(
+  '[data-testid="number-count"]'
+);
+const numberSumElement = document.querySelector('[data-testid="number-sum"]');
+const wordLengthAverageElement = document.querySelector(
+  '[data-testid="word-length-average"]'
+);
+const resetButton = document.getElementById("reset-button");
+
+resetButton.addEventListener("click", function () {
   if (textarea.value === "") {
-    alert("Debes ingresar algun texto para poder analizarlo.");
+    alert("Debes escribir algo para que pueda ser analizado.");
   } else {
     textarea.value = "";
+    metricas();
   }
 });
+
+textarea.addEventListener("input", function () {
+  metricas();
+});
+
+function metricas() {
+  const texto = textarea.value;
+  const wordCount = analyzer.getWordCount(texto);
+  const characterCount = analyzer.getCharacterCount(texto);
+  const characterNoSpacesCount =
+    analyzer.getCharacterCountExcludingSpaces(texto);
+  const numberCount = analyzer.getNumberCount(texto);
+  const numberSum = analyzer.getNumberSum(texto);
+  const wordLengthAverage = analyzer.getAverageWordLength(texto);
+
+  wordCountElement.textContent = `Palabras: ${wordCount}`;
+  characterCountElement.textContent = `Caracteres: ${characterCount}`;
+  characterNoSpacesCountElement.textContent = `Caracteres Sin Espacio: ${characterNoSpacesCount}`;
+  numberCountElement.textContent = `Números: ${numberCount}`;
+  numberSumElement.textContent = `Suma Números: ${numberSum}`;
+  wordLengthAverageElement.textContent = `Promedio Longitud: ${wordLengthAverage.toFixed(
+    2
+  )}`;
+}
+
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
